3241ac7bfa0daf728823fc820b10776bd1cdc2f7
rafinhaa <rafinhaa@me.com>
2021-11-07 15:33:10 -0300

    Chapter I - Conceitos importantes - Aula 05

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,60 @@
 import React from "react"; // Sempre usar o React para usar a sintaxe do JSX
-import {View, Text} from "react-native";
+import {View, 
+  Text, 
+  StyleSheet, 
+  TextInput, 
+  Platform,
+  TouchableOpacity,
+  } from "react-native";
 
 export function Home() {
   return (
-    <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
-      <Text>Hello World!</Text>
-      <Text>Hello World!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo, Rafael!</Text>
+      <TextInput style={styles.input} placeholder="Digite seu nome" placeholderTextColor="#555" />
+      <TouchableOpacity 
+        style={styles.button}
+        activeOpacity={0.6}
+      >
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+      <Text style={[styles.title, {marginTop:50}]}>
+        My Skills
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121015",
+    paddingHorizontal: 30,
+    paddingVertical: 70,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#1f1e25",
+    color: "#fff",
+    fontSize: 18,
+    padding: Platform.OS === "ios" ? 20 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: "#A370F7",
+    padding: 15,
+    borderRadius: 7,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
